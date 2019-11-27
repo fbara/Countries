ee9b3b8d7d515588c3be4b47a31e26cf7aeff24a
Frank S. Bara <fbara1@yahoo.com>
2019-11-27 12:40:03 -0600

    Initial Commit

---
+++ Countries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countries
+//
+//  Created by Frank Bara on 11/27/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
